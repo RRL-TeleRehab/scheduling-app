68e243d7d12b28a4eb1477736dc4de2741214b77
G Rahul <90084850+grahul27@users.noreply.github.com>
2022-07-26 22:55:20 -0600

    Update clinician Info

--- controllers/user.js
+++ controllers/user.js
@@ -17,7 +17,29 @@ exports.read = (req, res, next) => {
 };
 
 exports.update = (req, res, next) => {
-  const { firstName, lastName, password, confirmPassword } = req.body;
+  const {
+    firstName,
+    lastName,
+    newPassword,
+    confirmNewPassword,
+    title,
+    profilePhoto,
+    aboutClinician,
+    clinicContact,
+    dateOfBirth,
+    yearsOfExperience,
+    gender,
+    clinicName,
+    clinicRegisteredYear,
+    clinicRegistrationNo,
+    clinicianTrainedLocation,
+    clinicianProfessionalCourses,
+    clinicianSpecialization,
+    clinicAddress,
+    affiliatedFrom,
+    meetingLink,
+    socialMediaHandles,
+  } = req.body;
   const userId = req.user._id;
   User.findById(userId).exec((err, user) => {
     if (err || !user) {
@@ -32,14 +54,67 @@ exports.update = (req, res, next) => {
       user.lastName = lastName;
     }
 
-    if (password || confirmPassword) {
-      if (password !== confirmPassword) {
+    if (newPassword || confirmNewPassword) {
+      if (newPassword !== confirmNewPassword) {
         return res.status(400).json({
           error: "Passwords do not match",
         });
       } else {
-        user.password = password;
+        user.password = newPassword;
       }
+    }
+
+    if (aboutClinician) {
+      user.aboutClinician = aboutClinician;
+    }
+    if (affiliatedFrom) {
+      user.affiliatedFrom = affiliatedFrom;
+    }
+    if (title) {
+      user.title = title;
+    }
+    if (profilePhoto) {
+      user.profilePhoto = profilePhoto;
+    }
+
+    if (dateOfBirth) {
+      user.dateOfBirth = dateOfBirth;
+    }
+    if (yearsOfExperience) {
+      user.yearsOfExperience = yearsOfExperience;
+    }
+    if (gender) {
+      user.gender = gender;
+    }
+    if (clinicName) {
+      user.clinicName = clinicName;
+    }
+    if (clinicRegisteredYear) {
+      user.clinicRegisteredYear = clinicRegisteredYear;
+    }
+    if (clinicRegistrationNo) {
+      user.clinicRegistrationNo = clinicRegistrationNo;
+    }
+    if (clinicianTrainedLocation) {
+      user.clinicianTrainedLocation = clinicianTrainedLocation;
+    }
+    if (clinicAddress) {
+      user.clinicAddress = clinicAddress;
+    }
+    if (clinicContact) {
+      user.clinicContact = clinicContact;
+    }
+    if (meetingLink) {
+      user.meetingLink = meetingLink;
+    }
+    if (socialMediaHandles) {
+      user.socialMediaHandles = socialMediaHandles;
+    }
+    if (clinicianProfessionalCourses) {
+      user.clinicianProfessionalCourses = clinicianProfessionalCourses;
+    }
+    if (clinicianSpecialization) {
+      user.clinicianSpecialization = clinicianSpecialization;
     }
 
     user.save((err, updatedUser) => {
